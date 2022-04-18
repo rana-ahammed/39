c64cb61d3b00463d9e07bc82392fd776cf5212ce
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-04-18 13:11:52 +0600

    upto 39/2 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Shipment from './components/Shipment/Shipment';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count Value: {count}</p>
+      <Header count={count} setCount={setCount}></Header>
+      <Home count={count}></Home>
+      <Shipment></Shipment>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = (props) => {
+  const {count} = props;
+  return (
+    <div>
+      <h1>This is Home: {count} </h1> 
+    </div>
+  );
+};
+
+export default Home;
